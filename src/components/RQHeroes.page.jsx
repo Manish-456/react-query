2797267b0a87093bef94ecf1f5e1a78ae2d5e665
Manish <tamangmanish3456@gmail.com>
2023-11-02 11:55:46 +0545

    data transformation

--- src/components/RQHeroes.page.jsx
+++ src/components/RQHeroes.page.jsx
@@ -1,10 +1,21 @@
 import axios from "axios";
-import React from "react";
+import React, { useState } from "react";
 import { useQuery } from "react-query";
 
 const fetchHeroes = () => axios.get("http://localhost:3000/heroes");
 
 export default function RQHeroes() {
+  // const [refetchTime, setRefetchTime] = useState(3000);
+
+  const onSuccess = (data) => {
+    console.log(`Perform side effect after data fetching`, data);
+  };
+
+  const onError = (error) => {
+    // setRefetchTime(0);
+    console.log(`Perform side effect after encountering error`, error);
+  };
+
   const { data, isLoading, isError, error, refetch, isFetching } = useQuery(
     "heroes",
     fetchHeroes,
@@ -13,8 +24,14 @@ export default function RQHeroes() {
       staleTime: 0, // default to 0
       refetchOnMount: true, // default to true,
       refetchOnWindowFocus: true, // default to true
-      // refetchInterval : false 
-      enabled : false
+      // refetchInterval : refetchTime,
+      // enabled: false,
+      onSuccess,
+      onError,
+      select : ({data}) => { // select is a function that automatically receives an api data as an argument.
+        const heroNames = data.map(hero => hero.name);
+        return heroNames;
+      }  
     }
   );
 
@@ -25,13 +42,19 @@ export default function RQHeroes() {
   return (
     <div>
       <h2>RQ Heroes</h2>
-      <button type="button" onClick={refetch} >Fetch heroes</button>
-      {data?.data.map((hero) => (
-        <div key={hero.id} className="lists">
-          <h3>{hero.name}</h3>
-          <p>{hero.alterEgo}</p>
+      {/* <button type="button" onClick={refetch}>
+        Fetch heroes
+      </button> */}
+      {data?.map((heroName) => (
+        <div key={heroName} className="lists">
+          <h3>{heroName}</h3>
         </div>
       ))}
+      {/* {data?.data.map((hero) => (
+        <div key={hero.id} className="lists">
+          <h3>{hero.name}</h3>
+        </div>
+      ))} */}
     </div>
   );
 }
