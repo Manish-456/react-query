55f26146a942f4240a2bbb30d57688a3248e3cf5
Manish <tamangmanish3456@gmail.com>
2023-11-06 13:42:56 +0545

    optimistic updates

--- src/hooks/useHeroesData.js
+++ src/hooks/useHeroesData.js
@@ -20,19 +20,39 @@ export const useHeroesData = (onSuccess, onError) => {
 };
 
 const addHeroData = (values) => {
-  return axios.post(`http://localhost:3000/heroes`, values);
+  return axios.post(`http://localhost:3000/heroesd`, values);
 };
 export const useAddHeroData = () => {
   const queryClient = useQueryClient();
   return useMutation(addHeroData, {
-    onSuccess: (data) => {
-      // queryClient.invalidateQueries({queryKey: "heroes"}) // Avoid the additional network requests
-      queryClient.setQueryData("heroes", (oldData) => {
-        return {
-          ...oldData,
-          data: [...oldData.data, data.data],
-        };
+    // onSuccess: (data) => {
+    //   // queryClient.invalidateQueries({queryKey: "heroes"}) // Avoid the additional network requests
+    //   queryClient.setQueryData("heroes", (oldData) => {
+    //     return {
+    //       ...oldData,
+    //       data: [...oldData.data, data.data],
+    //     };
+    //   });
+    // },
+    onMutate : (newHero) => {
+      queryClient.cancelQueries("heroes");
+      const previousHeroData = queryClient.getQueryData("heroes");
+      queryClient.setQueryData("heroes", (oldQueryData) => {
+      return {
+        ...oldQueryData,
+        data : [...oldQueryData.data, {
+          id : oldQueryData?.data?.length + 1,
+          ...newHero
+        }]
+      }
       });
+      return previousHeroData;
     },
+    onError : (_error, _hero, context) => {
+      queryClient.setQueryData('heroes', context.previousHeroData);
+    },
+    onSettled : () => {
+      queryClient.invalidateQueries('heroes');
+    }
   });
 };
